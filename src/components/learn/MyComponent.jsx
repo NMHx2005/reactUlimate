a4716b48bed0136de45568cace3bdab45446d29a
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-10-22 14:26:31 +0700

    vd #30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,17 @@
+// JSX
+// Fragment
+
+import './style.css'
+
 const MyComponent = () => {
     return (
-        <div>NMH học React</div>
+        <>
+            <div>NMH học React</div>
+            <h2
+                className="child"
+
+            >MR.Hung</h2>
+        </>
     );
 }
 

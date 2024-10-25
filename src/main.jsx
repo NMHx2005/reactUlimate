7af5c33094c7840deeac857cd1b08a922f8e51d8
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-10-25 23:06:18 +0700

    vd #45

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/register",
+    element: <div>Đăng kí tài khoản</div>
+  },
+  {
+    path: "/login",
+    element: <div>Đăng Nhập</div>
+  },
+  {
+    path: "/contact",
+    element: <div>Liên hệ</div>
+  },
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
